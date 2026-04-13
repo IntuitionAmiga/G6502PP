be81d563273238e2d26c73869ac3b12c42d6154b
0xABADCAFE <karlchurchill@gmail.com>
2026-04-13 17:02:15 +0100

    Shift, Rotate and Bit operations

--- src/main.go
+++ src/main.go
@@ -10,34 +10,25 @@ func main() {
     const (
         BASE  = 0x200
         BLOCK = 32768
-        LOOPS = 1
+        LOOPS = 10000
     )
 
     mem := new(AddressSpace)
     mem[VEC_RES]     = BASE >> 8
     mem[VEC_RES + 1] = BASE & 0xFF
     
+    mem[0xFF00] = RTS;
+        
+    var i int
+    
     // Block of NOPS
-    for i:= 0; i < BLOCK; i++ {
-        mem[BASE + i] = NOP
+    for i = 0; i <= BLOCK; i += 3 {
+        mem[BASE + i] = JSR_AB
+        mem[BASE + i + 1] = 0x00
+        mem[BASE + i + 2] = 0xFF
     }
-    mem[BASE + BLOCK] = BEQ
-    mem[BASE + BLOCK + 1] = 0x4
-    mem[BASE + BLOCK + 2] = 0x0F
-    mem[BASE + BLOCK + 3] = 0x1F
-    mem[BASE + BLOCK + 4] = 0x2F
-    mem[BASE + BLOCK + 5] = 0x3F
-    mem[BASE + BLOCK + 6] = 0x4F
-    mem[BASE + BLOCK + 7] = 0x5F
-    mem[BASE + BLOCK + 8] = 0x6F
-    mem[BASE + BLOCK + 9] = 0x8F
-    mem[BASE + BLOCK + 10] = 0x9F
-    mem[BASE + BLOCK + 11] = 0xAF
-    mem[BASE + BLOCK + 12] = 0xBF
-    mem[BASE + BLOCK + 13] = 0xCF
-    mem[BASE + BLOCK + 14] = 0xDF
-    mem[BASE + BLOCK + 15] = 0xEF
-    mem[BASE + BLOCK + 16] = 0xBF
+
+    mem[BASE + i] = 0xFF // illegal    
 
     cpu := &MOS6502{}
     cpu.Init(mem).ShowStatus();
@@ -51,7 +42,7 @@ func main() {
     fMIPS := 1.0e3 * float64(LOOPS * BLOCK) / float64(iNanoSeconds)
 
     fmt.Printf(
-        "%d loops of %d NOP Took %d nanoseconds [%f MIPS]\n",
+        "%d loops of %d operations Took %d nanoseconds [%f MIPS]\n",
         LOOPS,
         BLOCK,
         iNanoSeconds,
